4006154aec1f1a88539817b4cfc3c1493af68922
79969 <m.byckoff2015@yandex.ru>
2023-07-03 23:33:16 +0300

    initial commit

---
+++ src/main/java/com/footsim/FootSimApiApplication.java
@@ -0,0 +1,13 @@
+package com.footsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootSimApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootSimApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/footsim/FootSimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootSimApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

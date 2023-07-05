6b22faa7f8da91c2d9168e3632dedaff71ba36fc
79969 <m.byckoff2015@yandex.ru>
2023-07-05 14:22:54 +0300

    feat: initialize service layer with CRUD operations

---
+++ src/main/java/com/footsim/service/impl/SeasonServiceImpl.java
@@ -0,0 +1,90 @@
+package com.footsim.service.impl;
+
+
+import com.footsim.domain.dto.SeasonDTO;
+import com.footsim.domain.model.Season;
+import com.footsim.mapper.SeasonMapper;
+import com.footsim.repository.SeasonRepository;
+import com.footsim.service.SeasonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Service Implementation for managing {@link Season}.
+ */
+@Service
+@Transactional
+public class SeasonServiceImpl implements SeasonService {
+
+    private final Logger log = LoggerFactory.getLogger(SeasonServiceImpl.class);
+
+    private final SeasonRepository seasonRepository;
+
+    private final SeasonMapper seasonMapper;
+
+
+    public SeasonServiceImpl(SeasonRepository seasonRepository, SeasonMapper seasonMapper) {
+        this.seasonRepository = seasonRepository;
+        this.seasonMapper = seasonMapper;
+    }
+
+    @Override
+    public SeasonDTO save(SeasonDTO seasonDTO) {
+        log.debug("Request to save Season : {}", seasonDTO);
+        Season season = seasonMapper.toEntity(seasonDTO);
+        season = seasonRepository.save(season);
+        return seasonMapper.toDto(season);
+    }
+
+    @Override
+    public SeasonDTO update(SeasonDTO seasonDTO) {
+        log.debug("Request to update Season : {}", seasonDTO);
+        Season season = seasonMapper.toEntity(seasonDTO);
+        season = seasonRepository.save(season);
+        return seasonMapper.toDto(season);
+    }
+
+    @Override
+    public Optional<SeasonDTO> partialUpdate(SeasonDTO seasonDTO) {
+        log.debug("Request to partially update Season : {}", seasonDTO);
+        return seasonRepository
+                .findById(seasonDTO.getId())
+                .map(existingSeason -> {
+                    seasonMapper.partialUpdate(existingSeason, seasonDTO);
+
+                    return existingSeason;
+                })
+                .map(seasonRepository::save)
+                .map(seasonMapper::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<SeasonDTO> findAll() {
+        log.debug("Request to get all Seasons");
+        return seasonRepository.findAll().stream().map(seasonMapper::toDto).collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public
+    SeasonDTO findOne(Long id) {
+        log.debug("Request to get Season : {}", id);
+        return seasonRepository.findById(id).map(seasonMapper::toDto).orElse(null);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete Season : {}", id);
+        seasonRepository.deleteById(id);
+    }
+
+
+}

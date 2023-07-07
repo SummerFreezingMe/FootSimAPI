e177f9f751fed3c3a39e7e332091c6a59ad5a638
79969 <m.byckoff2015@yandex.ru>
2023-07-07 23:51:12 +0300

    feat: add simple method for team's rating count

--- src/main/java/com/footsim/service/TeamService.java
+++ src/main/java/com/footsim/service/TeamService.java
@@ -55,4 +55,6 @@ public interface TeamService {
      * @param id the id of the entity.
      */
     void delete(Long id);
+
+    TeamDTO countTeamRating(Long id);
 }
--- src/main/java/com/footsim/service/impl/TeamServiceImpl.java
+++ src/main/java/com/footsim/service/impl/TeamServiceImpl.java
@@ -1,8 +1,11 @@
 package com.footsim.service.impl;
 
 import com.footsim.domain.dto.TeamDTO;
+import com.footsim.domain.enumeration.PlayerStatus;
+import com.footsim.domain.model.Player;
 import com.footsim.domain.model.Team;
 import com.footsim.mapper.TeamMapper;
+import com.footsim.repository.PlayerRepository;
 import com.footsim.repository.TeamRepository;
 import com.footsim.service.TeamService;
 import org.slf4j.Logger;
@@ -25,14 +28,17 @@ public class TeamServiceImpl implements TeamService {
     private final Logger log = LoggerFactory.getLogger(TeamServiceImpl.class);
 
     private final TeamRepository teamRepository;
-
+    private final PlayerRepository playerRepository;
     private final TeamMapper teamMapper;
 
 
 
 
-    public TeamServiceImpl(TeamRepository teamRepository, TeamMapper teamMapper) {
+    public TeamServiceImpl(TeamRepository teamRepository,
+                           PlayerRepository playerRepository,
+                           TeamMapper teamMapper) {
         this.teamRepository = teamRepository;
+        this.playerRepository = playerRepository;
         this.teamMapper = teamMapper;
     }
 
@@ -70,7 +76,8 @@ public class TeamServiceImpl implements TeamService {
     @Transactional(readOnly = true)
     public List<TeamDTO> findAll() {
         log.debug("Request to get all Teams");
-        return teamRepository.findAll().stream().map(teamMapper::toDto).collect(Collectors.toCollection(LinkedList::new));
+        return teamRepository.findAll().stream().map(teamMapper::toDto)
+                .collect(Collectors.toCollection(LinkedList::new));
     }
 
     @Override
@@ -78,7 +85,8 @@ public class TeamServiceImpl implements TeamService {
     public
     TeamDTO findOne(Long id) {
         log.debug("Request to get Team : {}", id);
-        return teamRepository.findById(id).map(teamMapper::toDto).orElse(null);
+        return teamRepository.findById(id).map(teamMapper::toDto)
+                .orElse(null);
     }
 
     @Override
@@ -87,5 +95,20 @@ public class TeamServiceImpl implements TeamService {
         teamRepository.deleteById(id);
     }
 
+    @Override
+    public TeamDTO countTeamRating(Long id) {
+        log.debug("Request to count Team rating: {}", id);
+        Team team = teamRepository.findById(id).orElseThrow();
+        List<Player> teamPlayers = playerRepository.findByClubId(id);
+        Long newTeamRating = 0L;
+        for (Player p: teamPlayers) {
+            if (p.getStatus()== PlayerStatus.ROSTER){
+                newTeamRating+=p.getRating();
+            }
+        }
+        team.setRating(newTeamRating);
+        teamRepository.save(team);
+        return teamMapper.toDto(team);
 
+    }
 }

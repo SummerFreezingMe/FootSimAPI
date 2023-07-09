955e72067aa525c9d92bfa24510665cf7121022e
79969 <m.byckoff2015@yandex.ru>
2023-07-09 22:23:07 +0300

    chore: add OpenApi setup

---
+++ src/main/java/com/footsim/config/SpringFoxConfig.java
@@ -0,0 +1,21 @@
+package com.footsim.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger configuration.
+ */
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("city-concert")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}

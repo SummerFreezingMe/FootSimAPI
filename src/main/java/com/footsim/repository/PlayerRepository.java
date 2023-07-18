dde0efe02f982fe910d56b58be526833a3f4ccdb
79969 <m.byckoff2015@yandex.ru>
2023-07-18 20:17:45 +0300

    fix: add PlayerStatus fields to queries in isRosterVariable method

--- src/main/java/com/footsim/repository/PlayerRepository.java
+++ src/main/java/com/footsim/repository/PlayerRepository.java
@@ -3,6 +3,7 @@ package com.footsim.repository;
 import com.footsim.domain.enumeration.PlayerPosition;
 import com.footsim.domain.enumeration.PlayerStatus;
 import com.footsim.domain.model.Player;
+import com.footsim.domain.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,12 +15,20 @@ import java.util.List;
 @SuppressWarnings("unused")
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    /**
+     * Find all players in a distinct {@link Team}
+     * @param id ID of a team
+     * @return List of Players
+     */
 
     List<Player> findByClubId(Long id);
 
-    Integer countPlayerByClubId(Long clubId);
 
-    Integer countPlayerByClubIdAndPosition(Long clubId, PlayerPosition playerPosition);
+    Integer countPlayerByClubIdAndStatus(Long clubId, PlayerStatus roster);
+
+    Integer countPlayerByClubIdAndPositionAndStatus(Long clubId,
+                                                    PlayerPosition playerPosition,
+                                                    PlayerStatus roster);
 
     List<Player> findByClubIdAndStatus(Long homeTeamId, PlayerStatus roster);
 }

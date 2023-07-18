b3633818897cf4e225091a1007863b20ba6d0b7b
79969 <m.byckoff2015@yandex.ru>
2023-07-18 21:14:54 +0300

    docs: complete Javadoc generation for a enums

--- src/main/java/com/footsim/domain/enumeration/FoulType.java
+++ src/main/java/com/footsim/domain/enumeration/FoulType.java
@@ -1,10 +1,30 @@
 package com.footsim.domain.enumeration;
 
+import com.footsim.domain.model.Foul;
+
+/**
+ * Type of suffered {@link Foul} enumeration
+ */
 public enum FoulType {
+    /**
+     * Light foul without serious penalty
+     */
     NO_CARD,
+    /**
+     * Average foul, culprit gets a yellow card as a penalty, second yellow card means send-off
+     */
     YELLOW_CARD,
+
+    /**
+     * Severe foul, culprit gets a red card and a send-off as a penalty
+     */
     RED_CARD;
 
+    /**
+     * Generates foul type depending on a numeric event possibility
+     * @param possibility floating point number from 0 to 1
+     * @return {@link FoulType}
+     */
     public static FoulType getType(double possibility){
         if(possibility<0.85&&possibility>=0){
             return NO_CARD;

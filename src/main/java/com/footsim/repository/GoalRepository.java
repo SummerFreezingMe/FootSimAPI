86ce8b00a0b6291a77faaeb8a77e6e89f7a1022e
79969 <m.byckoff2015@yandex.ru>
2023-07-25 23:27:54 +0300

    feat: add query methods for fetching top scorers and assistants

--- src/main/java/com/footsim/repository/GoalRepository.java
+++ src/main/java/com/footsim/repository/GoalRepository.java
@@ -1,8 +1,14 @@
 package com.footsim.repository;
 
+import com.footsim.domain.dto.TopActionsDTO;
 import com.footsim.domain.model.Goal;
+import com.footsim.domain.model.Player;
+import com.footsim.domain.model.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data JPA repository for the {@link Goal} entity.
@@ -10,5 +16,22 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
-
+    /**
+     * @param seasonId id of a {@link Season}
+     * @return List of id's of a {@link Player} and amount of their goals
+     */
+    @Query(value = "select player.id, count(goal.author_id)" +
+            "            from player join goal" +
+            "            on player.id=goal.author_id" +
+            " group by player.id",nativeQuery = true)
+    List<TopActionsDTO> findTopScorers(Long seasonId);
+    /**
+     * @param seasonId id of a {@link Season}
+     * @return List of id's of a {@link Player} and amount of their assists
+     */
+    @Query(value = "select player.id, count(goal.assist_id)" +
+            "            from player join goal" +
+            "            on player.id=goal.author_id" +
+            " group by player.id",nativeQuery = true)
+    List<TopActionsDTO> findTopAssistants(Long seasonId);
 }

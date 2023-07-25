c3d0e90250d2ce4b479bc99111eb7c1d07bceec8
79969 <m.byckoff2015@yandex.ru>
2023-07-25 22:33:36 +0300

    feat: add DTO for action tops of a season

---
+++ src/main/java/com/footsim/domain/dto/TopActionsDTO.java
@@ -0,0 +1,29 @@
+package com.footsim.domain.dto;
+
+import com.footsim.domain.model.Player;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * A DTO for the top scorers and top assistants.
+ */
+@SuppressWarnings("common-java:DuplicatedBlocks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TopActionsDTO {
+
+    /**
+     * Id of a {@link Player}
+     */
+    private Long id;
+
+    /**
+     * Amount of goals or assists of a player
+     */
+    private Short actions;
+
+}
